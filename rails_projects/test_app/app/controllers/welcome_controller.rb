44a9508432f8a5179bbfcd7e2885876188321d59
Kyle Maggard <kjm0001@gmail.com>
2017-02-02 13:20:27 +0000

    added rails-projects

---
+++ rails_projects/test_app/app/controllers/welcome_controller.rb
@@ -0,0 +1,15 @@
+# CamelCase class name
+# WelcomeController extends from ApplicationController (inherits from)
+class WelcomeController < ApplicationController
+  
+  # action
+  def home
+    
+  end
+  # action 
+  def about
+    
+  end
+  
+  
+end  
